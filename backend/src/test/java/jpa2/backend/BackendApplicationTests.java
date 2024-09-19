b40876f349c7211e711d7272641f62b8c8a3abcb
hl3470 <hl3470@naver.com>
2024-09-19 09:08:41 +0900

    init commit

---
+++ backend/src/test/java/jpa2/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa2.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
